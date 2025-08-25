559bdcabc97421e37b3ee99fbf4bab4dfb5c5eb6
Manoj <manokumar83954@gmail.com>
2025-08-25 09:36:06 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,20 @@
+a=int(input())
+print(a)
+
+#Datatypes
+
+b=int(input('Integer: '))
+print(type(b))
+
+c=float(input('Floating point number: '))
+print(type(c))
+
+d=input('String: ')
+print(type(d))
+
+e = input("Boolean value (True/False): ")
+e = e.lower() == "true"
+
+
+f=complex(input('complex value: '))
+print(type(f))
