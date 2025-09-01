65f1846af234409b57ecbf5bc158f72fadbedfa4
Manoj <manokumar83954@gmail.com>
2025-09-01 22:08:06 +0530

    first commit

---
+++ day3.py
@@ -0,0 +1,11 @@
+#Question 1
+
+a=eval(input())
+s=(2**i for i in a)
+print(s)
+
+#Question 2
+
+a=input()
+b=a.split(' ')
+print(a,'\n',b)
---
+++ day7.py
@@ -0,0 +1,11 @@
+#Question 1
+
+a=int(input())
+b=len(str(a))
+print(b)
+
+#Question 2
+
+num = int(input("Enter a number: "))
+rev = int(str(num)[::-1])
+print("Reversed number:", rev)
